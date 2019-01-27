a2dd5dc709d768be3be8ad0d7599b0b1dd4f7589
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-01-27 13:12:22 +0900

    Install FirebaseUI Auth SDK and GetCurrentUser

--- app/src/main/java/com/github/tatsuhama/firebase_hands_on/LaunchActivity.kt
+++ app/src/main/java/com/github/tatsuhama/firebase_hands_on/LaunchActivity.kt
@@ -1,11 +1,13 @@
 package com.github.tatsuhama.firebase_hands_on
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LaunchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val user = FirebaseAuth.getInstance().currentUser
     }
 }
