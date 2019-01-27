77a62a2dc99fae845724ebd72647a7784ce53abd
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-01-27 12:45:41 +0900

    Add LaunchActivity

---
+++ app/src/main/java/com/github/tatsuhama/firebase_hands_on/LaunchActivity.kt
@@ -0,0 +1,11 @@
+package com.github.tatsuhama.firebase_hands_on
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LaunchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
