089f36e2e2a701087302dec475598e3c6769d274
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-01-27 23:17:01 +0900

    Add ImageItemViewHolder

---
+++ app/src/main/java/com/github/tatsuhama/firebase_hands_on/ImageItemViewHolder.kt
@@ -0,0 +1,24 @@
+package com.github.tatsuhama.firebase_hands_on
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+
+class ImageItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(imageItem: ImageItem) {
+        val imageView = itemView.findViewById<ImageView>(R.id.image)
+        val storageRef = FirebaseStorage.getInstance().reference
+        storageRef.child("images/${imageItem.fileName}").downloadUrl.addOnSuccessListener {
+            Glide.with(imageView.context).load(it).into(imageView)
+        }.addOnFailureListener {
+            // Handle any errors
+        }
+
+        itemView.findViewById<TextView>(R.id.fileNameText).text = imageItem.fileName
+    }
+
+}
