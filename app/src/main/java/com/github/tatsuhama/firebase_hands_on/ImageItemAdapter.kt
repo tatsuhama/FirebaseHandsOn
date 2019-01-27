2a9a15fb1baa7da70ee2641ec2be0834114b2f8a
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-01-27 23:18:00 +0900

    Add ImageItemAdapter

---
+++ app/src/main/java/com/github/tatsuhama/firebase_hands_on/ImageItemAdapter.kt
@@ -0,0 +1,30 @@
+package com.github.tatsuhama.firebase_hands_on
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.google.firebase.firestore.FirebaseFirestore
+
+class ImageItemAdapter : RecyclerView.Adapter<ImageItemViewHolder>() {
+
+    private var items = listOf<ImageItem>()
+
+    init {
+        val collectionReference = FirebaseFirestore.getInstance().collection("images")
+        collectionReference.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+            notifyDataSetChanged()
+            items = querySnapshot?.documents?.map { ImageItem.from(it) } ?: listOf()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageItemViewHolder {
+        return ImageItemViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_image, parent, false))
+    }
+
+    override fun getItemCount(): Int = items.count()
+
+    override fun onBindViewHolder(holder: ImageItemViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+}
