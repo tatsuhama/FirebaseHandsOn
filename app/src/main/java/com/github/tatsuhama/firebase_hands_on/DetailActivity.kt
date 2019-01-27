e3c8420fb844aaae9666b04b977dc5d88551b48f
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-01-27 15:50:25 +0900

    Display the image on Storage with Glide (OSS Library)

--- app/src/main/java/com/github/tatsuhama/firebase_hands_on/DetailActivity.kt
+++ app/src/main/java/com/github/tatsuhama/firebase_hands_on/DetailActivity.kt
@@ -1,12 +1,22 @@
 package com.github.tatsuhama.firebase_hands_on
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val storageRef = FirebaseStorage.getInstance().reference
+        storageRef.child("images/sudachikun.jpg").downloadUrl.addOnSuccessListener {
+            Glide.with(this).load(it).into(image)
+        }.addOnFailureListener {
+            // Handle any errors
+        }
     }
 }
