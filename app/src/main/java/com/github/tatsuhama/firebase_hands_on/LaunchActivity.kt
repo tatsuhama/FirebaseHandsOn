48f010c9be18183e950987956f09896ddb868657
kenichi.tatsuhama <kenichi.tatsuhama@gmail.com>
2019-01-27 13:21:00 +0900

    Call AuthUI when not logged in

--- app/src/main/java/com/github/tatsuhama/firebase_hands_on/LaunchActivity.kt
+++ app/src/main/java/com/github/tatsuhama/firebase_hands_on/LaunchActivity.kt
@@ -2,6 +2,7 @@ package com.github.tatsuhama.firebase_hands_on
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 
 class LaunchActivity : AppCompatActivity() {
@@ -9,5 +10,22 @@ class LaunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            // Choose authentication providers
+            val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+            // Create and launch sign-in intent
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN
+            )
+        }
+    }
+
+    companion object {
+        private const val RC_SIGN_IN = 1
     }
 }
